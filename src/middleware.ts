5dd6732429c0725c42754395d09aed41c599954a
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2024-01-17 23:51:31 +0630

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,5 +28,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-	matcher: "/api/migration/:path*",
+	matcher: ["/api/migration/:path*", "/api/results/:path*"],
 };
